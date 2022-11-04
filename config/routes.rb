9da04a373a9779bcbce890b6a3d5061a8393f7e2
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-04 20:36:56 +0200

    w2e3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
+  get 'signup', to: 'users#new'
+
   root "breweries#index"
 end
