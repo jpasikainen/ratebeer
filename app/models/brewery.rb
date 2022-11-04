5383a5f896f07397965e4b4b9a8fa45852104301
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-04 17:49:13 +0200

    w2e14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ApplicationRecord
     has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
+
+    def average_rating
+        ratings.average :score
+    end
 end
