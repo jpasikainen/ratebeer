b14398c2e0741331ebfa396c264a4f452a5bc99d
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-04 15:23:57 +0200

    w2e7

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'ratings', to: 'ratings#index'
+  get 'ratings/new', to: 'ratings#new'
+
+  post 'ratings', to: 'ratings#create'
+
   # Defines the root path route ("/")
   root "breweries#index"
 end
