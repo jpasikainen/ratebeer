e4cca8e68c20bf72a755613a117f387e6a867bac
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-04 16:23:32 +0200

    w2e10

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -14,6 +14,7 @@ class BeersController < ApplicationController
   def new
     @breweries = Brewery.all
     @beer = Beer.new
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Lowalcohol"]
   end
 
   # GET /beers/1/edit
