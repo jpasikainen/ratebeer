9401e36cee1ce0524554890fd67807b94be2f11c
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-06 10:56:49 +0200

    w3e7

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,5 +2,10 @@ class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validates :name, length: { minimum: 1 }
+  validates :year, numericality: { greater_than_or_equal_to: 1040,
+                                   less_than_or_equal_to: 2022,
+                                   only_integer: true }
+
   include RatingAverage
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  include RatingAverage
   has_many :ratings
+
+  validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
+
+  include RatingAverage
 end
