b93bfaaa82755b5f2a51c174a0cb86487090abc3
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-06 21:06:50 +0200

    w3e15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   include RatingAverage
   has_secure_password
 
-  has_many :memberships
-  has_many :ratings
+  has_many :memberships, dependent: :destroy
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
   has_many :beer_clubs, through: :memberships
 
