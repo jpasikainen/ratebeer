7517599d1c3292aa42b3761541441427bf3980a4
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-06 20:51:37 +0200

    w3e13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user == current_user && @user.update(user_params)
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -52,8 +52,13 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
+      if @user == current_user
+        format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to users_url, notice: "User was unsccessfully destroyed." }
+        format.json { head :no_content }
+      end
     end
   end
 
