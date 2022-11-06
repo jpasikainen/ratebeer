d9f54630aa3b73a98e4555124126655d8d34ba1d
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-06 19:30:45 +0200

    w3e9-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  include RatingAverage
+
+  has_many :memberships
   has_many :ratings
+  has_many :beers, through: :ratings
+  has_many :beer_clubs, through: :memberships
 
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 30 }
-
-  include RatingAverage
 end
