38b45eb71b5cd5c324f143d95e2032759141ec6d
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-07 11:06:09 +0200

    w4e2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,6 +6,7 @@ class Beer < ApplicationRecord
   has_many :raters, through: :ratings, source: :user
 
   validates :name, length: { minimum: 1 }
+  validates :style, length: { minimum: 1 }
 
   def to_s
     "#{name} #{brewery.name}"
---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Beer, type: :model do
+  describe "creation" do
+    let(:test_brewery) { Brewery.new name: "test", year: 2000 }
+
+    it "succeeds with name, style and brewery" do
+      beer = Beer.create name: "beer", style: "cool", brewery: test_brewery
+      expect(beer).to be_valid
+      expect(Beer.count).to eq(1)
+    end
+
+    it "fails without a name" do
+      beer = Beer.create style: "cool", brewery: test_brewery
+      expect(beer).to_not be_valid
+    end
+
+    it "fails without a style" do
+      beer = Beer.create name: "beer", brewery: test_brewery
+      expect(beer).to_not be_valid
+    end
+  end
+end
