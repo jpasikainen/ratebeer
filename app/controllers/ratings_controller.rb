36c5d7087e24ebb81fddad41d072f413b1675d1b
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-13 20:05:23 +0200

    w6e8

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,7 +3,8 @@ class RatingsController < ApplicationController
     @ratings = Rating.all
     @recent_ratings = Rating.recent
     @top_beers = Rating.all.group_by(&:beer).sort_by{ |beer, _ratings| -beer.average_rating }.take(3)
-    @top_breweries = Rating.all.group_by{ |rating| rating.beer.brewery}.sort_by{ |brewery, _ratings| -brewery.average_rating }.take(3)
+    @top_breweries = Rating.all.group_by{ |rating| rating.beer.brewery }.sort_by{ |brewery, _ratings| -brewery.average_rating }.take(3)
+    @top_styles = Rating.all.group_by{ |rating| rating.beer.style }.sort_by{ |style, _ratings| -style.average_rating }.take(3)
   end
 
   def new
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,6 @@
 class Style < ApplicationRecord
   has_many :beers
+  has_many :ratings, through: :beers
+
+  include RatingAverage
 end
