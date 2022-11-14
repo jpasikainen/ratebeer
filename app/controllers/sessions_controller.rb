250ef5902b2690797700928bd5fe16c617fb3222
jpasikainen <asikainen.juhapekka@gmail.com>
2022-11-14 18:17:11 +0200

    w7e14

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by username: params[:username]
-    if user.banned
+    if user && user.banned
       return redirect_to signin_path, notice: "Your account is closed. Please contact admins."
     end
 
